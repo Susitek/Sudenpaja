f01cb8a8b28b763ff5c3d1defa66c7f5ba310fca
Susitek <susi.brofeldt@outlook.com>
2025-08-02 01:41:30 +0300

    Delete src directory

--- src/translations/fi.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  title: "Sudenpaja – 3D-tulostusta, rakentelua ja ratkaisuja",
-  description: "Sudenpaja tarjoaa laadukasta 3D-tulostuspalvelua ja rakenteluratkaisuja Lahdessa.",
-  welcome: "Tervetuloa Sudenpajaan!",
-  intro: "Olemme erikoistuneet 3D-tulostukseen, elektroniikkaan ja FPV-droneihin.",
-  aboutTitle: "Tietoa meistä",
-  aboutDescription: "Lisätietoja Sudenpajasta ja palveluistamme.",
-  aboutContent: "Sudenpaja on monipuolinen teknologiaharrastajien työpaja Lahdessa."
-};
